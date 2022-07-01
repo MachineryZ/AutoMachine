9aef7c6ffa19db75c682dbf84c38bfd629906e5e
MachineryZ <zhizuozhang@gmail.com>
2022-07-01 16:09:31 +0800

    update

---
+++ FactorBaseFunction/ts_sum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <Rcpp.h>
+using namespace Rcpp;
+
+NumericVector ts_sum(
+    Rcpp::NumericVector x,
+    const int window,
+    Rcpp::NumericVector weight,
+    const bool intercept,
+    const bool sign,
+    const bool skip,
+    const bool partial,
+    const int least,
+    const bool fill,
+    const int threads,
+    const int last, 
+)
+{
+
+}
+
+int main()
+{
+
+    return 0;
+}
