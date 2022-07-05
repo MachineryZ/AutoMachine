95d834e818a9d248d7cb109ddd3da63b502c6049
MachineryZ <zhizuozhang@gmail.com>
2022-07-05 09:54:46 +0800

    ts_median

--- FactorBaseFunction/ts_median.cpp
+++ FactorBaseFunction/ts_median.cpp
@@ -25,63 +25,43 @@ Rcpp::NumericVector ts_median(
     std::multiset<double> min_set;
     std::multiset<double> max_set;
     for (int i = 0; i < x_size; i++) {
-        if (i <= least - 1)
-            continue;
-        else if ((partial == true and i >= least - 1) or i >= window) {
-            // insert x[i]
-            if (max_set.size() > 0 and x[i] < *(max_set.begin()))
-                min_set.insert(x[i]);
+        // insert x[i]
+        if (max_set.size() > 0 and x[i] < *(max_set.begin()))
+            min_set.insert(x[i]);
+        else {
+            max_set.insert(x[i]);
+        }
+
+        // check size
+        if (min_set.size() == max_set.size() + 2) {
+            double max_min = *(--min_set.end());
+            min_set.erase(--min_set.end());
+            max_set.insert(max_min);
+        }
+        else if(min_set.size() == max_set.size() - 2) {
+            double max_min = *(max_set.begin());
+            max_set.erase(max_set.begin());
+            min_set.insert(max_min);
+        }
+
+        // erase element which is out of window
+        if (min_set.size() + max_set.size() > window) {
+            if ((min_set.size() > 0) and (x[i - window + 1] <= *(--min_set.end()))) {
+                min_set.erase(x[i - window + 1]);
+            }
             else {
-                max_set.insert(x[i]);
+                max_set.erase(x[i - window + 1]);
             }
+        }
 
-            // check size
-            if (min_set.size() == max_set.size() + 2) {
-                double max_min = *(--min_set.end());
-                min_set.erase(--min_set.end());
-                max_set.insert(max_min);
-            }
-            else if(min_set.size() == max_set.size() - 2) {
-                double max_min = *(max_set.begin());
-                max_set.erase(max_set.begin());
-                min_set.insert(max_min);
-            }
-
-            if (window % 2 == 0) {
+        if ((partial == true and i >= least - 1) or i >= window) {
+            if ((min_set.size() + max_set.size()) % 2 == 0) {
                 ret[i] = double((*(--min_set.end()) + *(max_set.begin())))/2;
             }
             else if (window % 2 == 1) {
                 ret[i] = min_set.size() > max_set.size() ? *(--min_set.end()) : *(max_set.begin());
             }
-
         }
-        if (i >= window - 1) {
-            // erase x[i - least + 1]
-            if ((min_set.size() > 0) and (x[i - window + 1] < *(--min_set.end())))
-                min_set.erase(x[i - window + 1]);
-            else 
-                max_set.erase(x[i - window + 1]);
-        }
-
-        // if (min_set.size() == max_set.size() + 2) {
-        //     double max_min = *(--min_set.end());
-        //     min_set.erase(*--min_set.end());
-        //     max_set.insert(max_min);
-        // }
-        // else if(min_set.size() == max_set.size() - 2) {
-        //     double max_min = *(max_set.begin());
-        //     max_set.erase(max_set.begin());
-        //     min_set.insert(max_min);
-        // }
-        std::cout << "min_set"; 
-        for (auto it = min_set.begin(); it != min_set.end(); it++) {
-            std::cout << *it << " ";
-        } std::cout << std::endl;
-        std::cout << "max_set"; 
-        for (auto it = max_set.begin(); it != max_set.end(); it++) {
-            std::cout << *it << " ";
-        } std::cout << std::endl << std::endl;
-
     }
     return ret;
 }
