670798c13b56804d36ac36ef3b361343a279d29e
MachineryZ <zhizuozhang@gmail.com>
2022-07-08 15:29:54 +0800

    1

---
+++ barraoptimizer/test.cpp
@@ -0,0 +1,6 @@
+virtual void BARRAOPT::CAsset::SetPowerTransactionCost(double c, double p, double q)
+Sets the power transaction cost function.
+参数:
+c – Transaction cost when trading value is q.
+p – The exponent of the power function; p>1.
+q – Trading value when the transaction cost is c.
