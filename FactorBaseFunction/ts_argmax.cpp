685a352bb47f85d97431916980b93de497dc576c
MachineryZ <zhizuozhang@gmail.com>
2022-07-08 17:29:46 +0800

    update

--- FactorBaseFunction/ts_argmax.cpp
+++ FactorBaseFunction/ts_argmax.cpp
@@ -1,4 +1,6 @@
 #include <Rcpp.h>
+#include <set>
+#include <bits/stdc++.h>
 
 //[[Rcpp::export]]
 Rcpp::NumericVector ts_argmax(
@@ -18,24 +20,37 @@ Rcpp::NumericVector ts_argmax(
         throw std::range_error("window must be a positive integer");
     
     Rcpp::NumericVector ret(x_size, fill);
-
-    for (int i = window - 1; i < x_size; i++) {
-        ret[i] = 0;
-        for (int j = i; j > i - window; j--) {
-            if (x[j] > x[i - window + 1 + ret[i]])
-                ret[i] = j - i + window - 1;
+    std::multiset<pair<int, double>> try_set;
+    for (int i = 0; i < x_size; i++) {
+        m_set.insert(make_pair(x[i], i));
+        if (m_set.size() > window) {
+            m_set.erase(m_set.find(make_pair(x[i - window], i - window)));
+        }
+        if (partial == true and (least - 1 <= i and i < window - 1)) {
+            ret[i] = m_set.begin().second;
+        }
+        else {
+            ret[i] = *m_set.begin();
         }
     }
 
-    if (partial == true) {
-        for (int i = least - 1; i < window - 1; i++) {
-            ret[i] = 0;
-            for (int j = i; j >= 0; j--) {
-                if (x[j] > x[ret[i]])
-                    ret[i] = j;
-            }
-        }
-    }
+    // for (int i = window - 1; i < x_size; i++) {
+    //     ret[i] = 0;
+    //     for (int j = i; j > i - window; j--) {
+    //         if (x[j] > x[i - window + 1 + ret[i]])
+    //             ret[i] = j - i + window - 1;
+    //     }
+    // }
+
+    // if (partial == true) {
+    //     for (int i = least - 1; i < window - 1; i++) {
+    //         ret[i] = 0;
+    //         for (int j = i; j >= 0; j--) {
+    //             if (x[j] > x[ret[i]])
+    //                 ret[i] = j;
+    //         }
+    //     }
+    // }
     return ret;
 }
 // library("Rcpp")
