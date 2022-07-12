0db767faa9bf34c45387d9bd9c853df08a1aa8f1
MachineryZ <zhizuozhang@gmail.com>
2022-07-12 14:23:15 +0800

    modify ts_argmax into O(N logN) time complexity

--- FactorBaseFunction/ts_argmax.cpp
+++ FactorBaseFunction/ts_argmax.cpp
@@ -20,39 +20,22 @@ Rcpp::NumericVector ts_argmax(
         throw std::range_error("window must be a positive integer");
     
     Rcpp::NumericVector ret(x_size, fill);
-    std::multiset<pair<int, double>> try_set;
+    std::multiset<std::pair<double, int>> m_set;
     for (int i = 0; i < x_size; i++) {
-        m_set.insert(make_pair(x[i], i));
+        m_set.insert(std::make_pair(x[i], i));
         if (m_set.size() > window) {
-            m_set.erase(m_set.find(make_pair(x[i - window], i - window)));
+            m_set.erase(m_set.find(std::make_pair(x[i - window], i - window)));
         }
         if (partial == true and (least - 1 <= i and i < window - 1)) {
-            ret[i] = m_set.begin().second;
+            ret[i] = (--m_set.end())->second;
         }
-        else {
-            ret[i] = *m_set.begin();
+        else if (i >= window - 1) {
+            ret[i] = (--m_set.end())->second - (i - window + 1);
         }
     }
-
-    // for (int i = window - 1; i < x_size; i++) {
-    //     ret[i] = 0;
-    //     for (int j = i; j > i - window; j--) {
-    //         if (x[j] > x[i - window + 1 + ret[i]])
-    //             ret[i] = j - i + window - 1;
-    //     }
-    // }
-
-    // if (partial == true) {
-    //     for (int i = least - 1; i < window - 1; i++) {
-    //         ret[i] = 0;
-    //         for (int j = i; j >= 0; j--) {
-    //             if (x[j] > x[ret[i]])
-    //                 ret[i] = j;
-    //         }
-    //     }
-    // }
     return ret;
 }
 // library("Rcpp")
 // sourceCpp(file="ts_argmax.cpp")
-// print(ts_argmax(c(1,5,4,3,5,4,10,2,2,2,1,1), 3, TRUE, 1))
+// print(ts_argmax(c(1,2,3,4,3), 3, TRUE, 1))
+// print(ts_argmax(c(1,5,4,3,5,4,10,2,2,2,1,1), 3, TRUE, 2))
