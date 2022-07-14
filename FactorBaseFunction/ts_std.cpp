7a940180d7d0d4551ffb9f50aab529a9af3bccce
MachineryZ <zhizuozhang@gmail.com>
2022-07-14 13:51:47 +0800

    modify ts_std

--- FactorBaseFunction/ts_std.cpp
+++ FactorBaseFunction/ts_std.cpp
@@ -2,10 +2,10 @@
 #include <cmath>
 
 //[[Rcpp::export]]
-Rcpp::NumericVector ts_stddev(
+Rcpp::NumericVector ts_std(
     const Rcpp::NumericVector& x,
     const int window,
-    const int least = 0,
+    const int least = 2,
     const bool partial = false,
     const double fill = NA_REAL
 ) {
@@ -20,42 +20,21 @@ Rcpp::NumericVector ts_stddev(
     double sum_x = 0.0;
     double square_sum_x = 0.0;
 
-    for (int i = 0; i < x_size: i++) {
+    for (int i = 0; i < x_size; i++) {
         sum_x += x[i];
         square_sum_x += x[i] * x[i];
         if (i > window - 1) {
             sum_x -= x[i];
             square_sum_x -= x[i] * x[i];
         }
+        if (i >= window - 1) {
+            ret[i] = std::sqrt((square_sum_x - sum_x * sum_x / (window)) / (window - 1));
+        }
+        else if (partial == true and i >= least - 1) {
+            ret[i] = std::sqrt((square_sum_x - sum_x * sum_x / (i + 1)) / i);
+        }
     }
-
-    // for (int i = window - 1; i < x_size; i++) {
-    //     double mean = 0.0;
-    //     ret[i] = 0.0;
-    //     for (int j = i; j > i - window; j--) {
-    //         mean += x[j];
-    //     }
-    //     mean = mean / window;
-    //     for (int j = i; j > i - window; j--) {
-    //         ret[i] += (x[j] - mean) * (x[j] - mean);
-    //     }
-    //     ret[i] = std::sqrt(ret[i] / (window - 1));
-    // }
-    // if (partial == true) {
-    //     for (int i = least - 1; i < window - 1; i++) {
-    //         double mean = 0.0;
-    //         ret[i] = 0.0;
-    //         for (int j = i; j >= 0; j--) {
-    //             mean += x[j];
-    //         }
-    //         mean = mean / window;
-    //         for (int j = i; j >= 0; j--) {
-    //             ret[i] += (x[j] - mean) * (x[j] - mean);
-    //         }
-    //         ret[i] = std::sqrt(ret[i] / i);
-    //     }
-    // }
-    // return ret;
+    return ret;
 }
 // library("Rcpp")
 // sourceCpp(file="ts_std.cpp")
