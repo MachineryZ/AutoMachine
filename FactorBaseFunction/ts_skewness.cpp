037aa22d8e2b432b4bbc9cd1d39d920fd9203245
MachineryZ <zhizuozhang@gmail.com>
2022-07-14 17:37:53 +0800

    add ts_skewness

--- FactorBaseFunction/ts_skewness.cpp
+++ FactorBaseFunction/ts_skewness.cpp
@@ -5,7 +5,7 @@ Rcpp::NumericVector ts_skewness(
     const Rcpp::NumericVector& x,
     const int window,
     const bool partial = false,
-    const int least = 0,
+    const int least = 2,
     const double fill = NA_REAL
 ) {
     int x_size = x.size();
@@ -15,38 +15,32 @@ Rcpp::NumericVector ts_skewness(
         throw std::range_error("window must be a positive integer");
 
     Rcpp::NumericVector ret(x_size, fill);
+    double sum_x = 0;
+    double square_sum_x = 0.0;
+    double cube_sum_x = 0.0;
 
-    for (int i = window - 1; i < x_size; i++) {
-        double mean_x = 0.0;
-        double sigma_x = 0.0;
-        double k_x = 0.0;
-        for (int j = i; j > i - window; j--) {
-            mean_x += x[j];
+    for (int i = 0; i < x_size; i++) {
+        sum_x += x[i];
+        square_sum_x += x[i] * x[i];
+        cube_sum_x += x[i] * x[i] * x[i];
+        if (i > window - 1) {
+            sum_x -= x[i - window];
+            square_sum_x -= x[i - window] * x[i - window];
+            cube_sum_x -= x[i - window] * x[i - window] * x[i - window];
         }
-        mean_x /= window;
-        for (int j = i; j > i - window; j--) {
-            sigma_x += (mean_x - x[j]) * (mean_x - x[j]);
-            k_x += (x[j] - mean_x) * (x[j] - mean_x) * (x[j] - mean_x);
+        if (i >= window - 1) {
+            double sigma_x = std::sqrt((square_sum_x - sum_x * sum_x / window) / (window - 1));
+            double k_x = cube_sum_x - \
+                3 * square_sum_x * sum_x / window + \
+                2 * sum_x * sum_x * sum_x / window / window;
+            ret[i] = k_x / sigma_x / sigma_x / sigma_x / window;
         }
-        sigma_x = std::sqrt(sigma_x/(window - 1));
-        ret[i] = k_x / sigma_x / sigma_x / sigma_x / window;
-    }
-
-    if (partial == true) {
-        for (int i = least - 1; i < window - 1; i++) {
-            double mean_x = 0.0;
-            double sigma_x = 0.0;
-            double k_x = 0.0;
-            for (int j = i; j > i - window; j--) {
-                mean_x += x[j];
-            }
-            mean_x /= least;
-            for (int j = i; j > i - window; j--) {
-                sigma_x += (x[j] - mean_x) * (x[j] - mean_x);
-                k_x += (x[j] - mean_x) * (x[j] - mean_x) * (x[j] - mean_x);
-            }
-            sigma_x = std::sqrt(sigma_x / (i - 1));
-            ret[i] = k_x / sigma_x / sigma_x / sigma_x / i;
+        else if (partial == true and i >= least - 1) {
+            double sigma_x = std::sqrt((square_sum_x - sum_x * sum_x / (i + 1)) / i);
+            double k_x = cube_sum_x - \
+                3 * square_sum_x * sum_x / (i + 1) + \
+                2 * sum_x * sum_x * sum_x / (i + 1) / (i + 1);
+            ret[i] = k_x / sigma_x / sigma_x / sigma_x / (i + 1);
         }
     }
     return ret;
